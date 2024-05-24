a4404409a33e26de5a1f2eec6db29c49f9c44c76
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2024-05-24 15:03:52 -0500

    Update CRUD service content

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -4,27 +4,64 @@ import { UpdateContentDto } from './dto/update-content.dto';
 
 @Injectable()
 export class ContentService {
-  
   create(createContentDto: CreateContentDto) {
-    return 'This action adds a new content';
+    return createContentDto;
   }
 
   findAll() {
-    return `This action returns all content`;
+    return [
+      {
+        id: 1,
+        type: 'presentation',
+        format: 'PowerPoint',
+        title: 'Introduction to Biology',
+        description:
+          'A PowerPoint presentation covering the basics of biology.',
+        size: '2MB',
+        edition_type: 'first',
+      },
+      {
+        id: 2,
+        type: 'video',
+        format: 'MP4',
+        title: 'Photosynthesis Process',
+        description: 'A video explaining the process of photosynthesis.',
+        size: '500MB',
+        edition_type: 'HD',
+      },
+      {
+        id: 3,
+        type: 'document',
+        format: 'PDF',
+        title: 'Math Exercises',
+        description: 'A PDF document containing various math exercises.',
+        size: '1MB',
+        edition_type: 'standard',
+      },
+    ];
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} content`;
+    return {
+      id: 1,
+      type: 'presentation',
+      format: 'PowerPoint',
+      title: 'Introduction to Biology',
+      description: 'A PowerPoint presentation covering the basics of biology.',
+      size: '2MB',
+      edition_type: 'first',
+    };
   }
 
   update(id: number, updateContentDto: UpdateContentDto) {
-    return `This action updates a #${id} content`;
+    return updateContentDto;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} content`;
+    return id;
   }
+
   partialUpdate(id: number, updateContentDto: UpdateContentDto) {
-    return `This action partially updates a #${id} content`;
-}
+    return updateContentDto;
+  }
 }
